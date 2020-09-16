2cf026bfd1d5e6d4055c5ad20e75a93a3520b749
Y.W KIM <kyw624@gmail.com>
2020-09-17 00:48:54 +0900

    Skill 컴포넌트 완성

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,7 +1,147 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Icon } from 'semantic-ui-react';
+import SkillsItem from './SkillsItem';
 
-function Skills() {
-  return <div>Skills</div>;
+const SkillsContainer = styled.div`
+  margin: 0;
+  padding: 1rem 10rem;
+  padding-top: 2rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const SkillsWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1.5rem 0;
+`;
+
+const SkillsTitle = styled.div`
+  font-weight: bold;
+  font-size: 2.6rem;
+  border-bottom: 0.2rem solid ${(props) => props.color};
+  padding-bottom: 0.5rem;
+  margin-bottom: 0.7rem;
+`;
+
+const SkillsContent = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 14rem;
+`;
+
+const ArrowButton = styled.div`
+  margin: 0;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  display: flex;
+  align-items: center;
+  transition: all 1s ease-in-out;
+  animation: none;
+
+  &:first-child {
+    cursor: pointer;
+    color: ${(props) => props.color};
+    font-size: 5.2rem;
+  }
+
+  .clicked {
+    animation: opacityArrow 0.5s forwards;
+
+    @keyframes opacityArrow {
+      50% {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+function Skills(props) {
+  const SkillItems = [
+    {
+      caption: 'Language',
+      items: ['JavaScript', 'TypeScript', 'NodeJS', 'Python'],
+      ability: ['초급', '입문', '입문', '초급'],
+    },
+    {
+      caption: 'Web Design',
+      items: ['HTML', 'CSS', 'Sass'],
+      ability: ['초급', '초급', '초급'],
+    },
+    {
+      caption: 'Library',
+      items: ['ReactJS', 'Redux'],
+      ability: ['초급', '초급'],
+    },
+    {
+      caption: 'Database',
+      items: ['mongoDB', 'MySQL'],
+      ability: ['입문', '입문'],
+    },
+    {
+      caption: 'OS',
+      items: ['Windows', 'Linux'],
+      ability: ['숙련', '초급'],
+    },
+    {
+      caption: 'Etc',
+      items: ['Git', 'MS Office'],
+      ability: ['초급', '숙련'],
+    },
+  ];
+
+  const renderSkillItem = (arr) => {
+    return arr.map((item, index) => {
+      return (
+        <SkillsWrap key={index}>
+          <SkillsTitle color={props.color}>{item.caption}</SkillsTitle>
+          <SkillsContent>
+            <SkillsItem item={item} />
+          </SkillsContent>
+        </SkillsWrap>
+      );
+    });
+  };
+
+  const clickArrow = (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+
+    target.classList.remove('clicked');
+
+    if (target.classList.contains('right')) {
+      target.classList.add('left');
+      target.classList.remove('right');
+    } else {
+      target.classList.add('right');
+      target.classList.remove('left');
+    }
+
+    void target.offsetWidth; // 애니메이션 한 번만 실행되는 현상 방지
+
+    target.classList.add('clicked');
+    // 아이콘 변경시 트랜지션 줄 예정
+  };
+
+  return (
+    <SkillsContainer>
+      <ArrowButton color={props.color}>
+        <Icon name="angle double right" onClick={clickArrow} />
+      </ArrowButton>
+      {renderSkillItem(SkillItems)}
+    </SkillsContainer>
+  );
 }
 
-export default Skills;
+export default React.memo(Skills);
---
+++ src/components/Skills/SkillsItem.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ItemContainer = styled.div`
+  position: relative;
+  width: 10rem;
+  height: 12rem;
+  box-shadow: 0.2rem 0.4rem 1rem 0.2rem rgba(0, 0, 0, 0.3),
+    -0.2rem 0.4rem 1rem 0.2rem rgba(0, 0, 0, 0.3);
+  border-radius: 2rem;
+  overflow: hidden;
+  margin: 1rem;
+
+  &:after {
+    content: '${(props) => props.ability}';
+    color: white;
+    font-size: 2.6rem;
+    font-weight: bold;
+    line-height: 12rem;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.75);
+    z-index: 5;
+    position: absolute;
+    left: 0;
+    top: 0;
+    transition: all 0.15s ease-in-out;
+    opacity: 0;
+  }
+
+  &:hover:after {
+    opacity: 1;
+  }
+
+  img {
+    width: 10rem;
+    height: 10rem;
+    background: white;
+  }
+`;
+
+const TextBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 2rem;
+  left: 0;
+  bottom: 0;
+  background: black;
+  color: white;
+  line-height: 2rem;
+`;
+
+const SkillsItem = (props) => {
+  console.log(props.item);
+  const imageData = {
+    javascript: require('../../images/logo/javascript.png'),
+    typescript: require('../../images/logo/typescript.png'),
+    nodejs: require('../../images/logo/nodejs.png'),
+    python: require('../../images/logo/python.png'),
+    html: require('../../images/logo/html.png'),
+    css: require('../../images/logo/css.png'),
+    sass: require('../../images/logo/sass.png'),
+    reactjs: require('../../images/logo/reactjs.png'),
+    redux: require('../../images/logo/redux.png'),
+    mongodb: require('../../images/logo/mongodb.png'),
+    mysql: require('../../images/logo/mysql.png'),
+    windows: require('../../images/logo/windows.png'),
+    linux: require('../../images/logo/linux.png'),
+    git: require('../../images/logo/git.png'),
+    msoffice: require('../../images/logo/msoffice.png'),
+  };
+  const renderSkills = (arr) => {
+    return arr.items.map((item, index) => {
+      const imageName = item.replace(/\s*/g, '').toLowerCase(); // 공백제거 및 소문자
+      const ability = arr.ability[index];
+
+      return (
+        <ItemContainer key={index} ability={ability}>
+          <img src={imageData[imageName]} alt={imageName} />
+          <TextBox>{item}</TextBox>
+        </ItemContainer>
+      );
+    });
+  };
+
+  return <>{renderSkills(props.item, props.index)}</>;
+};
+
+export default SkillsItem;
