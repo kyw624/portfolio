69eb5960bc9a349d13b512fa32483ac9ec0b18df
Y.W KIM <kyw624@gmail.com>
2020-10-01 18:12:52 +0900

    Project 컴포넌트 완성

---
+++ src/components/Projects/ProjectContents.js
@@ -0,0 +1,119 @@
+import React, { useRef } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const boxFade = keyframes`
+    from {
+      opacity: 0;
+    }
+    to {  
+      opacity: 1;
+    }
+`;
+
+const ProjectDescription = styled.div`
+  position: relative;
+  padding: 2rem;
+  width: 49%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  border: 0.3rem solid ${(props) => props.color};
+  background: rgba(245, 253, 255, 0.85);
+  animation: ${boxFade} 1s forwards;
+`;
+
+const ProjectTitle = styled.div`
+  padding-bottom: 2rem;
+  border-bottom: 0.2rem solid ${(props) => props.color};
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  div {
+    font-size: 2.4rem;
+    opacity: 0.5;
+  }
+`;
+
+const ProjectContent = styled.div`
+  margin-top: 3rem;
+  font-weight: 600;
+
+  p {
+    margin-bottom: 4rem;
+    font-size: 2.2rem;
+  }
+
+  div {
+    margin: 2rem 0;
+    font-size: 2rem;
+  }
+`;
+
+const ProjectLink = styled.a`
+  position: absolute;
+  bottom: 2rem;
+  width: calc(100% - 4rem);
+  height: 5rem;
+  border: 0.1rem solid ${(props) => props.color};
+  border-radius: 1rem;
+  background: white;
+  align-self: center;
+  color: ${(props) => props.color};
+  font-size: 3rem;
+  text-align: center;
+  line-height: 4.8rem; /* border: -0.2rem */
+  cursor: pointer;
+
+  &:hover {
+    border: 0.1rem solid white;
+    background: ${(props) => props.color};
+    color: white;
+  }
+`;
+
+const ProjectImage = styled.img`
+  opacity: 0;
+  width: 49%;
+  height: 100%;
+  border: 0.3rem solid ${(props) => props.color};
+  animation: ${boxFade} 1s 1s forwards;
+`;
+
+function ProjectContents(props) {
+  const fadeRef = useRef();
+
+  void fadeRef.offsetWidth;
+
+  return (
+    <>
+      <ProjectDescription color={props.color} ref={fadeRef}>
+        <ProjectTitle color={props.color}>
+          <h2>{props.items.title}</h2>
+          <div>{props.items.subtitle}</div>
+        </ProjectTitle>
+        <ProjectContent>
+          <p>{props.items.description}</p>
+          <div>개발기간: {props.items.period}</div>
+          <div>사용기술: {props.items.skill}</div>
+          <div>기여도: {props.items.role}</div>
+        </ProjectContent>
+        <ProjectLink
+          color={props.color}
+          href={props.items.link}
+          target="_blank"
+        >
+          Link
+        </ProjectLink>
+      </ProjectDescription>
+      <ProjectImage
+        color={props.color}
+        ref={fadeRef}
+        src={props.items.thumbnail}
+      />
+    </>
+  );
+}
+
+export default ProjectContents;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,121 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import styled, { keyframes } from 'styled-components';
+import ProjectContents from './ProjectContents';
 
-function Projects() {
-  return <div>Projects</div>;
+const boxScale = keyframes`
+  from {
+    transform: none;
+  }
+  to {
+    transform: scale(1.1);
+  }
+`;
+
+const ProjectContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3rem;
+`;
+
+const ProjectWrap = styled.div`
+  width: 100%;
+  height: 85%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const PageContainer = styled.div`
+  border: 0.2rem solid ${(props) => props.color};
+  border-radius: 3rem;
+  width: 15%;
+  height: 10%;
+  background: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  animation: ${boxScale} 1s alternate infinite;
+
+  &:hover {
+    animation: none;
+  }
+`;
+
+const PageButton = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
+
+function Projects(props) {
+  const [item, setItem] = useState(0);
+
+  const changeProject = useCallback((num) => setItem(num), []);
+
+  const displayProject = useCallback((color, project, array) => {
+    return <ProjectContents color={color} items={array[project]} />;
+  }, []);
+
+  const renderPageButton = (arr) => {
+    return arr.map((item, index) => {
+      return (
+        <PageButton
+          key={index}
+          color={props.color}
+          onClick={() => {
+            changeProject(index);
+          }}
+        />
+      );
+    });
+  };
+
+  const projectItems = [
+    {
+      title: 'My Mate',
+      subtitle: '일정관리 웹 앱 (Vanilla JS)',
+      description:
+        'Web localStorage를 이용한 리스트 및 아이템 추가/제거, 중요 일정 추가/제거 기능이 있는 일정관리 페이지입니다.',
+      period: '2020-08-16 ~ 2020-09-01',
+      skill: 'HTML, CSS, JavaScript',
+      role: '100%',
+      thumbnail: require('../../images/thumbnail/my-mate.gif'),
+      link: 'https://kyw624.github.io/my-mate/',
+    },
+    {
+      title: 'Portfolio',
+      subtitle: '포트폴리오 웹 페이지 (React)',
+      description:
+        '리액트를 사용해 Mac OS UI를 컨셉으로 제작한 개인 포트폴리오 웹 페이지입니다.',
+      period: '2020-09-14 ~',
+      skill: 'React',
+      role: '100%',
+      thumbnail: '',
+      link: '',
+    },
+  ];
+
+  return (
+    <ProjectContainer>
+      <ProjectWrap color={props.color}>
+        {displayProject(props.color, item, projectItems)}
+      </ProjectWrap>
+      <PageContainer color={props.color}>
+        {renderPageButton(projectItems)}
+      </PageContainer>
+    </ProjectContainer>
+  );
 }
 
-export default Projects;
+export default React.memo(Projects);
