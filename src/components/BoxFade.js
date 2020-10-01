d4a021534bee21aee44f0011f23a700c10ea83ec
Y.W KIM <kyw624@gmail.com>
2020-10-01 23:28:05 +0900

    Fade 효과 컴포넌트화

---
+++ src/components/BoxFade.js
@@ -0,0 +1,12 @@
+import { keyframes } from 'styled-components';
+
+const BoxFade = keyframes`
+    from {
+      opacity: 0;
+    }
+    to {  
+      opacity: 1;
+    }
+`;
+
+export default BoxFade;
